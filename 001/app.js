cd824d470a18991df0836d36abe5086d527cd677
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-22 20:44:16 +0300

    map

---
+++ 001/app.js
@@ -0,0 +1,32 @@
+console.log('Labutis');
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// console.log(rand(5, 10));
+
+const mas = [...Array(5)].map(_ => rand(100, 999));
+
+console.log(mas);
+
+const fe = mas.forEach(e => 1);
+const map = mas.map(e => e + 1);
+
+console.log(fe);
+console.log(map);
+
+// const a = [4, 8, 7];
+
+// const naujasA = {...a};
+
+// const tasPatA = a;
+
+// naujasA.push(99);
+// tasPatA.push(77);
+// a.push(77);
+
+// console.log('A', a);
+// console.log(naujasA);
+// console.log(tasPatA);

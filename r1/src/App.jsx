883667632774f728d70d926316c786f06825c3f3
A-Kija <42836152+A-Kija@users.noreply.github.com>
2022-08-25 19:07:49 +0300

    list

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,17 +1,23 @@
 import './App.css';
-import Cat from './Components/003/Cat';
-import Racoon from './Components/003/Racoon';
-// import Dog from './Components/003/Dog';
+
+const cats = [
+    {id: 1, weight: 5, name: 'Murka'},
+    {id: 2, weight: 8, name: 'Pilkis'},
+    {id: 3, weight: 2, name: 'Pūkis'},
+    {id: 4, weight: 3, name: 'Rainis'},
+    {id: 5, weight: 5, name: 'Brisius'}
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Dog></Dog> */}
-        <h1>Start</h1>
-        {/* <Dog></Dog> */}
-        <Cat />
-        <Racoon />
+            <h1>Lists</h1>
+            <ul>
+                {
+                    [...cats].sort((a, b) => b.weight - a.weight).map((cat, i) => <li key={i}>{cat.name} <i>{cat.weight}kg</i></li>)
+                }
+            </ul>
       </header>
     </div>
   );

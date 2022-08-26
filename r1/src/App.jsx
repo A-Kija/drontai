1552160c19d84a50d7c386af9194423bfc94e4d1
A-Kija <42836152+A-Kija@users.noreply.github.com>
2022-08-26 18:50:43 +0300

    button click

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.scss';
 
 function App() {
+
+    const doJob = () => {
+        console.log('Hello');
+    }
+
+    const clicked = () => {
+        console.log('labas');
+        doJob();
+    }
+
+
     return (
         <div className="App">
             <header className="App-header">
             <h1>STATE</h1>
+            <button onClick={clicked}>CLICK!</button>
             </header>
         </div>
     );

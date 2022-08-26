8f5944a24ca52e1d3d2bc8fde2a373f643a33edc
A-Kija <42836152+A-Kija@users.noreply.github.com>
2022-08-26 19:47:41 +0300

    size

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,22 +1,39 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
 
-    const doJob = () => {
-        console.log('Hello');
-    }
+
+    const [color, setColor] = useState('crimson');
+    const [size, setSize] = useState(30);
+
+    /*
+    const color <--- state (props) 
+    const setColor <--- funkcija state keitimui
+    */
+
+    // const doJob = function() {
+    //     console.log('Hello');
+    // }
 
     const clicked = () => {
-        console.log('labas');
-        doJob();
+        setColor('skyblue');
+    }
+
+    const doSize = () => {
+        setSize(40);
     }
 
 
     return (
         <div className="App">
             <header className="App-header">
-            <h1>STATE</h1>
+            <h1 style={{
+                color: color,
+                fontSize: size + 'px'
+                }}>STATE</h1>
             <button onClick={clicked}>CLICK!</button>
+            <button onClick={doSize}>DO SIZE</button>
             </header>
         </div>
     );

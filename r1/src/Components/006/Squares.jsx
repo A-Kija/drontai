01487d15c999cd61c006eef297131f2cca467a32
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-29 19:15:34 +0300

    kv

--- r1/src/Components/006/Squares.jsx
+++ r1/src/Components/006/Squares.jsx
@@ -12,7 +12,7 @@ function Squares() {
         <>
         <div className="container">
             {
-                sq.map((_, i) => <div key={i}></div>)
+                sq.map((_, i) => <div key={i}>{ i + 1 }</div>)
             }
         </div>
             <button onClick={add}>add []</button>

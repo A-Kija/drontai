cd6e0a1a1365b1c2653b538c12485b9355813190
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-29 20:06:03 +0300

    sq

--- r1/src/Components/006/Squares.jsx
+++ r1/src/Components/006/Squares.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
+import rand from '../../Functions/rand';
 
 function Squares() {
 
     const [sq, setSq] = useState([]);
 
     const add = () => {
-        setSq(s => [...s, '']);
+        setSq(s => [...s, rand(100, 999)]);
     }
 
     return (
         <>
-        <div className="container">
-            {
-                sq.map((_, i) => <div key={i}>{ i + 1 }</div>)
-            }
-        </div>
+            <div className="container">
+                {
+                    sq.map((n, i) => <div key={i}>{n}</div>)
+                }
+            </div>
             <button onClick={add}>add []</button>
         </>
     )

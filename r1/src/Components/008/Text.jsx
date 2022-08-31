c9a47023f72e3684072b5f34562233278685fbb1
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-31 20:09:14 +0300

    color

--- r1/src/Components/008/Text.jsx
+++ r1/src/Components/008/Text.jsx
@@ -3,20 +3,29 @@ import { useState } from 'react';
 function Text() {
 
     const [text, setText] = useState('');
+    const [color, setColor] = useState('#7caa0c');
 
     const control = e => {
-        if (Number(e.target.value) == e.target.value) {
-            setText(e.target.value);
-        }
+        // Number(e.target.value) && setText(e.target.value);
+        setText(e.target.value);
     }
-    
-    return (
-        
 
-        <div className="form-container">
-            <h2>TEXT</h2>
-            <input type="text" onChange={control} value={text}></input>
-        </div>
+    const colorControl = e => {
+        setColor(e.target.value);
+    }
+
+    return (
+
+        <>
+            <div className="form-container">
+                <h2>TEXT</h2>
+                <input type="text" onChange={control} value={text}></input>
+            </div>
+            <div className="form-container">
+                <h2 style={{color}}>COLOR</h2>
+                <input type="color" onChange={colorControl} value={color}></input>
+            </div>
+        </>
     )
 }
 

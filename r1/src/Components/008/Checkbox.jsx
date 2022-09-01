d2826cf3e12ffbf117361dbcc408d17745e90381
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-01 20:01:31 +0300

    cb done

--- r1/src/Components/008/Checkbox.jsx
+++ r1/src/Components/008/Checkbox.jsx
@@ -6,8 +6,7 @@ function Checkbox() {
 
     const change = e => {
         const v = e.target.value;
-        setCb(c => ({...c, [v]: !c[v]}));
-        
+        setCb(c => ({...c, [v]: !c[v]})); //jei v = 'A' => A: !c.A
     }
 
     return (
@@ -15,19 +14,19 @@ function Checkbox() {
             <div className="form-container">
                 <div>
                     <input type="checkbox" value="A" id="_1" onChange={change} checked={cb.A}></input>
-                    <label htmlFor="_1">A Raidė</label>
+                    <label htmlFor="_1" style={{color: cb.A ? 'crimson' : null}}>A Raidė</label>
                 </div>
                 <div>
                     <input type="checkbox" value="B" id="_2" onChange={change} checked={cb.B}></input>
-                    <label htmlFor="_2">B Raidė</label>
+                    <label htmlFor="_2" style={{color: cb.B ? 'crimson' : null}}>B Raidė</label>
                 </div>
                 <div>
                     <input type="checkbox" value="C" id="_3" onChange={change} checked={cb.C}></input>
-                    <label htmlFor="_3">C Raidė</label>
+                    <label htmlFor="_3" style={{color: cb.C ? 'crimson' : null}}>C Raidė</label>
                 </div>
                 <div>
                     <input type="checkbox" value="D" id="_4" onChange={change} checked={cb.D}></input>
-                    <label htmlFor="_4">D Raidė</label>
+                    <label htmlFor="_4" style={{color: cb.D ? 'crimson' : null}}>D Raidė</label>
                 </div>
 
             </div>

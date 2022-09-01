45e474eebb94fa0f2fac556719a6a09df51cd80c
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-01 19:53:26 +0300

    cb

--- r1/src/Components/008/Checkbox.jsx
+++ r1/src/Components/008/Checkbox.jsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
+
 function Checkbox() {
+
+    const [cb, setCb] = useState({A: false, B: true, C: true, D: false});
+
+    const change = e => {
+        const v = e.target.value;
+        setCb(c => ({...c, [v]: !c[v]}));
+        
+    }
 
     return (
         <>
             <div className="form-container">
                 <div>
-                    <input type="checkbox" value="A" id="_1"></input>
+                    <input type="checkbox" value="A" id="_1" onChange={change} checked={cb.A}></input>
                     <label htmlFor="_1">A Raidė</label>
                 </div>
                 <div>
-                    <input type="checkbox" value="B" id="_2"></input>
+                    <input type="checkbox" value="B" id="_2" onChange={change} checked={cb.B}></input>
                     <label htmlFor="_2">B Raidė</label>
                 </div>
                 <div>
-                    <input type="checkbox" value="C" id="_3"></input>
+                    <input type="checkbox" value="C" id="_3" onChange={change} checked={cb.C}></input>
                     <label htmlFor="_3">C Raidė</label>
                 </div>
                 <div>
-                    <input type="checkbox" value="D" id="_4"></input>
+                    <input type="checkbox" value="D" id="_4" onChange={change} checked={cb.D}></input>
                     <label htmlFor="_4">D Raidė</label>
                 </div>
 

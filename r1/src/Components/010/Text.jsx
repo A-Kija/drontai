5d92cc6c7dc36787e6bcef3661f82eebb695da41
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-02 18:21:58 +0300

    text

---
+++ r1/src/Components/010/Text.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+function Text() {
+
+    const [text, setText] = useState('');
+    const [textNow, setTextNow] = useState('');
+
+    const control = e => {
+        setText(e.target.value);
+    }
+
+    return ( 
+        <>
+        <div className="form-container">
+            <h2>TEXT: {textNow}</h2>
+            <input type="text" onChange={control} value={text}></input>
+            <button onClick={() => setTextNow(text)}>Text Now</button>
+        </div>
+        </>
+    )
+}
+
+export default Text;

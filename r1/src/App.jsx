6e08e39a42e61e286917666082b973184e81c844
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-02 18:28:47 +0300

    state up

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.scss';
 import Text from './Components/010/Text';
+import { useState } from 'react';
 
 function App() {
 
-
+    const [textNow, setTextNow] = useState('');
 
     return (
         <div className="App">
             <header className="App-header">
                 <h1>State Uplifting</h1>
-                <Text />
+                <h2>TEXT: {textNow}</h2>
+                <Text setTextNow={setTextNow} />
             </header>
         </div>
     );

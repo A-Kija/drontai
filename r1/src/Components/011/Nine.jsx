ec16152adeee990e0050eeee656be64838bd0bc0
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-05 20:45:14 +0300

    ch color

--- r1/src/Components/011/Nine.jsx
+++ r1/src/Components/011/Nine.jsx
@@ -1,9 +1,14 @@
-function Nine({nine}) {
+import randomColor from '../../Functions/randColor';
+function Nine({nine, setNine}) {
+
+    const changeColor = c => {
+        setNine(n => n.map(kv => kv === c ? randomColor() : kv));
+    }
 
     return (
         <div className="container9">
             {
-                nine.map((c, i) => <div key={i} style={{backgroundColor: c}}></div>)
+                nine.map((c, i) => <div onClick={() => changeColor(c)} key={i} style={{backgroundColor: c}}></div>)
             }
         </div>
     );

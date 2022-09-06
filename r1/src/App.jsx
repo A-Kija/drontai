7ccff9f892e02513847fcbd5d96c46d72cf3005b
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-06 21:05:28 +0300

    stars

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.scss';
+import L1 from './Components/012/L1';
 
 function App() {
+
+    const [stars, setStars] = useState('');
 
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Context</h1>
-                
+                <L1 stars={stars} />
+                <button onClick={() => setStars(s => s + '*')}>ADD *</button>
             </header>
         </div>
     );

af72a617a630ce276c1a4d06d538ae32cfb2dad9
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-06 18:56:31 +0300

    localStorage

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,20 +1,37 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.scss';
 
 function App() {
 
-    const [counts, setCounts] = useState({one: 0, two: 0});
+    const [counts, setCounts] = useState(null);
 
-    
+    useEffect(() => {
+        const data = localStorage.getItem('counts_key');
+        if (null === data) {
+            setCounts({one: 0, two: 0});
+        } else {
+            setCounts(JSON.parse(data));
+        } 
+    }, []);
+
+    useEffect(() => {
+        if (null === counts) {
+            return;
+        }
+        localStorage.setItem('counts_key', JSON.stringify(counts));
+    }, [counts]);
 
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Local Store</h1>
-                <h2>ONE: {counts.one}</h2>
-                <h2>TWO: {counts.two}</h2>
+                <h2>ONE: {counts?.one}</h2>
+                <h2>TWO: {counts?.two}</h2>
                 <button onClick={() => setCounts(c => ({...c, one: c.one + 1}))}>One +</button>
                 <button onClick={() => setCounts(c => ({...c, two: c.two + 1}))}>Two +</button>
+                <button onClick={() => setCounts(c => ({...c, one: c.one - 1}))}>One -</button>
+                <button onClick={() => setCounts(c => ({...c, two: c.two - 1}))}>Two -</button>
             </header>
         </div>
     );

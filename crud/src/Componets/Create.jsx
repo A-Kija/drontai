833920e43b2bec79f0e290ef491a51d724689113
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-07 19:03:29 +0300

    sass 3

--- crud/src/Componets/Create.jsx
+++ crud/src/Componets/Create.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 function Create() {
+
+    const [thing, setThing] = useState('');
+    const [color, setColor] = useState('');
+    const [cs, setCs] = useState(false);
 
     return (
         <div className="card">
@@ -8,17 +14,20 @@ function Create() {
             <div className="body">
                 <div className="form">
                     <label>Thing</label>
-                    <input type="text"></input>
+                    <input type="text" value={thing} onChange={e => setThing(e.target.value)}></input>
                 </div>
                 <div className="form">
                     <label>Color</label>
-                    <input type="color"></input>
+                    <input type="color" value={color} onChange={e => setColor(e.target.value)}></input>
                 </div>
                 <div className="form">
-                    <input type="checkbox" id="sc"></input>
+                    <input type="checkbox" id="sc" checked={cs} onChange={() => setCs(c => !c)}></input>
                     <label htmlFor="sc">Circle or Square</label>
                     <div className="c"></div>
                     <div className="s"></div>
+                </div>
+                <div className="form">
+                    <button className="blue">Make Thing</button>
                 </div>
             </div>
         </div>

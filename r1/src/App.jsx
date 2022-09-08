bce7343b226b72f40cf7ea311dc0cf05b96e6f79
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-08 18:36:16 +0300

    T1

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,28 +1,14 @@
-import { useState } from 'react';
 import './App.scss';
-import DataPlus from './Components/012/DataPlus';
-import L1 from './Components/012/L1';
+import T1 from './Components/014/T1';
 
 function App() {
 
-    const [stars, setStars] = useState('');
-    const [pluses, setPluses] = useState('');
-    const [minuses, setMinuses] = useState('');
-
     return (
         <div className="App">
-            <DataPlus.Provider value={{
-                pluses: pluses,
-                minuses: minuses
-            }}>
             <header className="App-header">
-                <h1>Context</h1>
-                <L1 stars={stars} />
-                <button onClick={() => setStars(s => s + '*')}>ADD *</button>
-                <button onClick={() => setPluses(s => s + '+')}>ADD +</button>
-                <button onClick={() => setMinuses(s => s + '-')}>ADD -</button>
+            <h1>Total Recall 1</h1>
+            <T1 />
             </header>
-            </DataPlus.Provider>
         </div>
     );
 }

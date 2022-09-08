f522a4e3fe5eb6971bc485d89cb65c40bdef2c3d
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-08 21:08:56 +0300

    t5

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import T4 from './Components/014/T4';
+import T5 from './Components/014/T5';
 
 function App() {
 
@@ -7,7 +7,7 @@ function App() {
         <div className="App">
             <header className="App-header">
             <h1>Total Recall 1</h1>
-            <T4 />
+            <T5 />
             </header>
         </div>
     );

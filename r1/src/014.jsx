52699a41a7dda61f98f3ef3d1bc884db35fbd9c5
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-09 18:06:24 +0300

    start

--- r1/src/014.jsx
+++ r1/src/014.jsx
@@ -1,11 +1,13 @@
 import './App.scss';
+import T7 from './Components/014/T7';
+
 function App() {
 
     return (
         <div className="App">
             <header className="App-header">
-            <h1>Total Recall 2</h1>
-
+            <h1>Total Recall 1</h1>
+            <T7 />
             </header>
         </div>
     );

63a713e060e6be68480259778a8d830e9d85be98
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-15 20:08:17 +0300

    go

--- r1/src/Components/019/Books.jsx
+++ r1/src/Components/019/Books.jsx
@@ -4,10 +4,16 @@ import axios from 'axios';
 function Books()  {
 
     const [books, setBooks] = useState(null);
+    const [types, setTypes] = useState(null);
 
     useEffect( () => {
         axios.get('https://in3.dev/knygos/')
         .then(res => setBooks(res.data));
+    }, []);
+
+    useEffect( () => {
+        axios.get('https://in3.dev/knygos/types/')
+        .then(res => setTypes(res.data));
     }, []);
 
     if (null === books) {
@@ -22,10 +28,11 @@ function Books()  {
         <div className="books">
             {
                 books?.map(b => <div className="book" key={b.id}>
+                    <div className="types">{types?.find(t => b.id === t.id).title}</div>
                     <h2>{b.title}</h2>
                     <img src={b.img} alt={b.title}></img>
                     <h4>{b.author}</h4>
-                    <div className="price">{b.price} eur</div>
+                    <div className="price">{b.price.toFixed(2)} eur</div>
                 </div>)
             }
         </div>

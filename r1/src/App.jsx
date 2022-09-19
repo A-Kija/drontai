4813a4875844454420f69c8cc4c14c4d3225464c
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-19 19:57:40 +0300

    count2

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -5,10 +5,20 @@ import './App.scss';
 function App() {
 
     const [counter, setCounter] = useState(0);
+    const [counter2, setCounter2] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             setCounter(c => c + 1);
+        }, 1000);
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter2(c => c >= 10 ? 0 : c + 2);
         }, 1000);
         return () => {
             clearInterval(intervalId);
@@ -22,6 +32,7 @@ function App() {
         <div className="App">
             <header className="App-header">
             <h2>{counter}</h2>
+            <h2>{counter2}</h2>
             </header>
         </div>
     );

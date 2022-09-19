181e3d54c72c3388ffa03d65cbc9cdd3ddd00188
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-19 21:59:31 +0300

    count down

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     const [counter4, setCounter4] = useState([rand(10, 99), rand(10, 99)]);
     const [counter5, setCounter5] = useState(0);
     const [color, setColor] = useState('skyblue');
+    const [counter6, setCounter6] = useState('');
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -54,26 +55,35 @@ function App() {
     }, []);
 
     const changeColor = () => {
+        const intervalId = setInterval(() => {
+            setCounter6(c => {
+                const n = c === '' ? 9 : c - 1;
+                if (n === 0) {
+                    clearInterval(intervalId);
+                }
+                return n;
+            });
+        }, 100);
         setTimeout(() => {
-            setColor('crimson')
+            setColor('crimson');
         }, 1000);
     }
-    
-           
+
+
     return (
         <div className="App">
             <header className="App-header">
-            <h2>{counter}</h2>
-            <h2>{counter2}</h2>
-            <h2>{counter4[0]}-{counter4[1]} {counter5}</h2>
-            <div className="containerBin">
-                <div style={{backgroundColor:color}} onClick={changeColor}></div>
-            </div>
-            <div className="containerBin">
-                {
-                    [...Array(counter3)].map((_, i) => <div key={i}></div>)
-                }
-            </div>
+                <h2>{counter}</h2>
+                <h2>{counter2}</h2>
+                <h2>{counter4[0]}-{counter4[1]} {counter5}</h2>
+                <div className="containerBin">
+                    <div style={{ backgroundColor: color }} onClick={changeColor}>{counter6}</div>
+                </div>
+                <div className="containerBin">
+                    {
+                        [...Array(counter3)].map((_, i) => <div key={i}></div>)
+                    }
+                </div>
             </header>
         </div>
     );

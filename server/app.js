5ea16b5fdff675d6c333c095ef48853b8cf74ae8
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-20 20:39:57 +0300

    app done

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3003;
+const cors = require("cors");
+app.use(cors());
+const mysql = require("mysql");
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ruo_nei_ka",
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Labas, Briedi!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Bebras klauso ${port} porto`);
+});

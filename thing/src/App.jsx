ae4840ca3675ee5e28651025fa781137a24cf1a1
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-21 18:39:00 +0300

    up

--- thing/src/App.jsx
+++ thing/src/App.jsx
@@ -1,8 +1,30 @@
-import './App.scss';
+const textures = [
+  {id: 1, title: 'Wood'},
+  {id: 2, title: 'Metal'},
+  {id: 3, title: 'Paper'},
+  {id: 4, title: 'Stone'}
+]
 
 function App() {
+
+
+
   return (
-    <span>veikia</span>
+    <DataContext.Provider value={{
+      
+    }}>
+      <div className="container">
+        <div className="bin">
+          <div className="box-1">
+            {/* <Create />
+            <Bin /> */}
+          </div>
+          <div className="box-2">
+            <List />
+          </div>
+        </div>
+      </div>
+    </DataContext.Provider>
   );
 }
 

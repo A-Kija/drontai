5f4c5d50972a6cea9eb2532fe560c262b51b0b15
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-23 22:13:43 +0300

    Cheers!

--- thing/src/Components/drinks/Glass.jsx
+++ thing/src/Components/drinks/Glass.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 import rand from '../../Functions/rand';
 
-function Glass({id, empty}) {
+function Glass({id, empty, drinkType}) {
 
     const [fill, setFill] = useState(null);
     const speed = useRef(null);
@@ -22,16 +22,31 @@ function Glass({id, empty}) {
         } else {
             empty(id);
         }
-    }, [fill]);
+    }, [fill, empty, id]);
 
     const drink = () => {
         setFill(f => f ? f - 30 : 0);
     }
 
+    const doColor = drink => {
+        switch(drink) {
+            case 'Mojito':
+                return 'lime';
+            case 'Bloody Mary':
+                return 'crimson';
+            case 'Cuba Libre':
+                return 'rgb(43, 6, 6)';
+            default: return 'orange';
+        }
+    } 
+
 
     return (
         <div className="glass">
-            <div className="fill" style={{height: fill + 'px'}}>
+            <div className="fill" style={{
+                height: fill + 'px',
+                backgroundColor: doColor(drinkType)
+                }}>
             </div>
         </div>
     )
--- thing/src/Components/drinks/Main.jsx
+++ thing/src/Components/drinks/Main.jsx
@@ -1,18 +1,28 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import rand from "../../Functions/rand";
 import Glass from "./Glass";
+
+const menu = [
+    'Mojito',
+    'Bloody Mary',
+    'Cuba Libre'
+]
 
 function Main() {
 
     const [drinks, setDrinks] = useState(null);
 
     useEffect(() => {
-        setDrinks([...Array(19)].map(() => ({id: rand(1000000, 9999999)})));
+        setDrinks([...Array(19)].map(() => ({id: rand(1000000, 9999999), drink: menu[rand(0, 2)]})));
     }, []);
 
-    const empty = id => {
-        setDrinks(d => d.map(g => g.id === id ? {...g, id: rand(1000000, 9999999)} : {...g}));
-    }
+    const empty = useCallback(id => {
+        setDrinks(d => d.map(g => g.id === id ? {id: rand(1000000, 9999999), drink: menu[rand(0, 2)]} : {...g}));
+    }, []);
+
+    // const empty = id => {
+    //     setDrinks(d => d.map(g => g.id === id ? {...g, id: rand(1000000, 9999999)} : {...g}));
+    // }
 
     return (
         <div className="container bar">
@@ -20,7 +30,7 @@ function Main() {
                 <h1>Drinks Bar</h1>
                 <div className="table">
                     {
-                        drinks?.map(d => <Glass key={d.id} id={d.id} empty={empty} />)
+                        drinks?.map(d => <Glass key={d.id} id={d.id} empty={empty} drinkType={d.drink} />)
                     }
                 </div>
             </div>
